e9f36da39818da37867ea91384a3a9830f6bc4ae
michael-dev-pilotin <62141851+michael-dev-pilotin@users.noreply.github.com>
2021-03-05 10:54:31 +0100

    Update styles-option-configuration.php

--- includes/classes/admin/options-pages/styles-option-configuration.php
+++ includes/classes/admin/options-pages/styles-option-configuration.php
@@ -421,10 +421,10 @@ Please see TailwindCSS documentation for more details (if you have enabled this 
                 ),
             ),
 
-            // Colors with shades
+            // Colors variants
             array(
                 'key'                           => 'field_pip_colors_shades',
-                'label'                         => __( 'Colors with shades', 'pilopress' ),
+                'label'                         => __( 'Colors variants', 'pilopress' ),
                 'name'                          => 'pip_colors_shades',
                 'type'                          => 'repeater',
                 'instructions'                  => '',
@@ -441,7 +441,7 @@ Please see TailwindCSS documentation for more details (if you have enabled this 
                 'min'                           => 0,
                 'max'                           => 0,
                 'layout'                        => 'table',
-                'button_label'                  => __( 'Add color with shades', 'pilopress' ),
+                'button_label'                  => __( 'Add color variant', 'pilopress' ),
                 'sub_fields'                    => array(
                     array(
                         'key'               => 'field_colors_shades_color_name',
@@ -465,7 +465,7 @@ Please see TailwindCSS documentation for more details (if you have enabled this 
                     ),
                     array(
                         'key'                           => 'field_colors_shades_shades',
-                        'label'                         => __( 'Shades', 'pilopress' ),
+                        'label'                         => __( 'Variants', 'pilopress' ),
                         'name'                          => 'shades',
                         'type'                          => 'repeater',
                         'instructions'                  => '',
@@ -482,7 +482,7 @@ Please see TailwindCSS documentation for more details (if you have enabled this 
                         'min'                           => 0,
                         'max'                           => 0,
                         'layout'                        => 'table',
-                        'button_label'                  => __( 'Add shade', 'pilopress' ),
+                        'button_label'                  => __( 'Add variant', 'pilopress' ),
                         'sub_fields'                    => array(
                             array(
                                 'key'               => 'field_shade_label',
@@ -506,7 +506,7 @@ Please see TailwindCSS documentation for more details (if you have enabled this 
                             ),
                             array(
                                 'key'               => 'field_shade_name',
-                                'label'             => __( 'Shade name', 'pilopress' ),
+                                'label'             => __( 'Variant name', 'pilopress' ),
                                 'name'              => 'shade_name',
                                 'type'              => 'text',
                                 'instructions'      => __( '<code>500</code> in <code>text-gray-500</code>', 'pilopress' ),
