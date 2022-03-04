fcbc8d6f4b3a5aa52b5c928ddbba9ecf65fdb3e0
damien-pilot-in <damien@pilot-in.com>
2022-03-04 16:52:57 +0100

    Change "Target content" layout preview

--- includes/layouts/template-target-content/template-target-content.php
+++ includes/layouts/template-target-content/template-target-content.php
@@ -4,10 +4,12 @@ global $is_preview;
 // Show current post flexible content
 if ( !$is_preview ) :
     echo '%%PIP_LOCKED_CONTENT%%';
-else :
-    ?>
-    <div style="padding: 80px 20px;text-align: center;border: 2px dashed #aaa;">
-        <em style="color:#aaa; font-size: 16px"><?php _e( 'Target content (layouts)', 'pilot-in' ); ?></em>
+else : ?>
+    <div style="position: relative; padding: 40px 20px; text-align: center;">
+        <div style="position: absolute; left: 20px; right: 20px; top: -2%; height: 25%; border: 2px dashed #aaa;"></div>
+        <div style="border: 2px dashed #aaa; margin: 10px auto; padding: 20px;">
+            <em style="color:#aaa; font-size: 16px"><?php _e( 'Target content', 'pilot-in' ); ?></em>
+        </div>
+        <div style="position: absolute; left: 20px; right: 20px; top: auto; bottom: -2%; height: 25%; border: 2px dashed #aaa;"></div>
     </div>
-<?php endif; ?>
-<?php
+<?php endif;
